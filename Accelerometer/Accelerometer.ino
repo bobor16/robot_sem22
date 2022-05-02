4da746943cc678a28878e55e33aa776ae59ce923
Borgar Bordoy <bobor16@student.sdu.dk>
2022-05-02 16:44:39 +0200

    added working accelerometer

--- Accelerometer/Accelerometer.ino
+++ Accelerometer/Accelerometer.ino
@@ -1,115 +1,86 @@
-// Basic demo for accelerometer readings from Adafruit MPU6050
+// Basic OLED demo for accelerometer readings from Adafruit MPU6050
 
 // ESP32 Guide: https://RandomNerdTutorials.com/esp32-mpu-6050-accelerometer-gyroscope-arduino/
 // ESP8266 Guide: https://RandomNerdTutorials.com/esp8266-nodemcu-mpu-6050-accelerometer-gyroscope-arduino/
 // Arduino Guide: https://RandomNerdTutorials.com/arduino-mpu-6050-accelerometer-gyroscope/
 
 #include <Adafruit_MPU6050.h>
+#include <Adafruit_SSD1306.h>
 #include <Adafruit_Sensor.h>
-#include <Wire.h>
 
 Adafruit_MPU6050 mpu;
+Adafruit_SSD1306 display = Adafruit_SSD1306(128, 64, &Wire);
 
-void setup(void) {
-  Serial.begin(9600);
-  while (!Serial)
-    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+void setup() {
+  Serial.begin(115200);
+  // while (!Serial);
+  Serial.println("MPU6050 OLED demo");
 
-  Serial.println("Adafruit MPU6050 test!");
-
-  // Try to initialize!
   if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
-    while (1) {
-      delay(10);
-    }
+    Serial.println("Sensor init failed");
+    while (1)
+      yield();
   }
-  Serial.println("MPU6050 Found!");
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
-  case MPU6050_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case MPU6050_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case MPU6050_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case MPU6050_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
-  }
+  Serial.println("Found a MPU-6050 sensor");
 
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  Serial.print("Gyro range set to: ");
-  switch (mpu.getGyroRange()) {
-  case MPU6050_RANGE_250_DEG:
-    Serial.println("+- 250 deg/s");
-    break;
-  case MPU6050_RANGE_500_DEG:
-    Serial.println("+- 500 deg/s");
-    break;
-  case MPU6050_RANGE_1000_DEG:
-    Serial.println("+- 1000 deg/s");
-    break;
-  case MPU6050_RANGE_2000_DEG:
-    Serial.println("+- 2000 deg/s");
-    break;
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ; // Don't proceed, loop forever
   }
-
-  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
-  case MPU6050_BAND_260_HZ:
-    Serial.println("260 Hz");
-    break;
-  case MPU6050_BAND_184_HZ:
-    Serial.println("184 Hz");
-    break;
-  case MPU6050_BAND_94_HZ:
-    Serial.println("94 Hz");
-    break;
-  case MPU6050_BAND_44_HZ:
-    Serial.println("44 Hz");
-    break;
-  case MPU6050_BAND_21_HZ:
-    Serial.println("21 Hz");
-    break;
-  case MPU6050_BAND_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case MPU6050_BAND_5_HZ:
-    Serial.println("5 Hz");
-    break;
-  }
-
-  Serial.println("");
-  delay(100);
+  display.display();
+  delay(500); // Pause for 2 seconds
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setRotation(0);
 }
 
 void loop() {
-  /* Get new sensor events with the readings */
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
 
-  Serial.print("Acceleration X: ");
-  Serial.print(a.acceleration.x);
-  Serial.print(", Y: ");
-  Serial.print(a.acceleration.y);
-  Serial.print(", Z: ");
-  Serial.print(a.acceleration.z);
+  display.clearDisplay();
+  display.setCursor(0, 0);
+
+  Serial.print("Accelerometer ");
+  Serial.print("X: ");
+  Serial.print(a.acceleration.x, 1);
+  Serial.print(" m/s^2, ");
+  Serial.print("Y: ");
+  Serial.print(a.acceleration.y, 1);
+  Serial.print(" m/s^2, ");
+  Serial.print("Z: ");
+  Serial.print(a.acceleration.z, 1);
   Serial.println(" m/s^2");
 
-  Serial.print("Rotation X: ");
-  Serial.print(g.gyro.x);
-  Serial.print(", Y: ");
-  Serial.print(g.gyro.y);
-  Serial.print(", Z: ");
-  Serial.print(g.gyro.z);
-  Serial.println(" rad/s");
+  display.println("Accelerometer - m/s^2");
+  display.print(a.acceleration.x, 1);
+  display.print(", ");
+  display.print(a.acceleration.y, 1);
+  display.print(", ");
+  display.print(a.acceleration.z, 1);
+  display.println("");
 
-  Serial.println("");
-  delay(500);
+  Serial.print("Gyroscope ");
+  Serial.print("X: ");
+  Serial.print(g.gyro.x, 1);
+  Serial.print(" rps, ");
+  Serial.print("Y: ");
+  Serial.print(g.gyro.y, 1);
+  Serial.print(" rps, ");
+  Serial.print("Z: ");
+  Serial.print(g.gyro.z, 1);
+  Serial.println(" rps");
+
+  display.println("Gyroscope - rps");
+  display.print(g.gyro.x, 1);
+  display.print(", ");
+  display.print(g.gyro.y, 1);
+  display.print(", ");
+  display.print(g.gyro.z, 1);
+  display.println("");
+
+  display.display();
+  delay(100);
 }
